f25050cc5db762ab5a85b491383c57b9129e424b
chelahcynthia <cchelangat989@gmail.com>
2024-10-14 14:06:45 +0300

    match phone numbers

--- regex.py
+++ regex.py
@@ -28,3 +28,10 @@ if re.match(pattern,string):
     print('match founf')
 else:
     print('no match found')
+    
+# matching phone numbers 
+text ="Please contact me at +1 (123) 456-7890 or via emial at john@example.com"
+pattern =r"\+?\d{1,3}[-.\s]\(?\d{1,3}\)?[-.\s]?\d{1,3}[-.\s]?d{1,4}"
+ 
+matches = re.findall(pattern,text)
+print(matches)
