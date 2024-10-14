6315b6b236cdc653e2afcbb0e61d966dd527b510
chelahcynthia <cchelangat989@gmail.com>
2024-10-14 14:04:45 +0300

    match characters

--- regex.py
+++ regex.py
@@ -8,3 +8,23 @@ if re.match(pattern, string):
     print("match found")
 else:
     print("no match found")
+    
+
+# Wildcard
+pattern =r"a.b"
+string ="acbb"
+
+if re.match(pattern,string):
+    print('match found')
+else:
+    print('match not found')
+    
+
+# character
+pattern =r"[0-9]"
+string ="0123"
+
+if re.match(pattern,string):
+    print('match founf')
+else:
+    print('no match found')
